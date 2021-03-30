7c59daa9308990832f22420b84c778fe5efa4a32
LeonardoMazzoni <leonardomazzoni02@gmail.com>
2021-03-30 21:56:13 +0200

    il GET funziona

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Employee } from './tabella/tabella.component';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { retry, catchError } from 'rxjs/Operators';
 
 @Injectable({
@@ -13,7 +13,8 @@ export class RestService {
   handleError:any;
 
   getDataRows(apiURL: string): Observable<Employee> {
-    return this.http.get<Employee>(apiURL).pipe(
+    return this.http.get<Employee>(apiURL)
+    .pipe(
       retry(1),
       catchError(this.handleError)
     )
--- src/app/tabella/tabella.component.ts
+++ src/app/tabella/tabella.component.ts
@@ -9,24 +9,12 @@ export interface Employee {
   phone: string;
 }
 
-/* const dati: Employee[] = [
-  {employeeId: 1, firstName: 'Hydrogen', lastName: '1.0079', email: 'H', phone: 'sdj'},
-  {employeeId: 2, firstName: 'Helium', lastName: '4.0026', email: 'He', phone: 'sdj'},
-  {employeeId: 3, firstName: 'Lithium', lastName: '6.941', email: 'Li', phone: 'sdj'},
-  {employeeId: 4, firstName: 'Beryllium', lastName: '9.0122', email: 'Be', phone: 'sdj'},
-  {employeeId: 5, firstName: 'Boron', lastName: '10.811', email: 'B', phone: 'sdj'},
-  {employeeId: 6, firstName: 'Carbon', lastName: '12.0107', email: 'C', phone: 'sdj'},
-  {employeeId: 7, firstName: 'Nitrogen', lastName: '14.0067', email: 'N', phone: 'sdj'},
-  {employeeId: 8, firstName: 'Oxygen', lastName: '15.9994', email: 'O', phone: 'sdj'},
-  {employeeId: 9, firstName: 'Fluorine', lastName: '18.9984', email: 'F', phone: 'sdj'},
-  {employeeId: 10, firstName: 'Neon', lastName: '20.1797', email: 'Ne', phone: 'sdj'},
-]; */
-
 @Component({
   selector: 'app-tabella',
   styleUrls: ['tabella.component.css'],
   templateUrl: 'tabella.component.html',
 })
+
 export class TabellaComponent {
   constructor(private restClient: RestService) {
     this.loadData();
@@ -42,5 +30,5 @@ export class TabellaComponent {
       error => this.errors = error
     )
   }
-
 }
+
